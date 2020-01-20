92909905b62f7e0e8e8df1e5bcf806dbb1c90311
Marcus Andersson <marcus.andersson@grafana.com>
2020-01-20 12:43:39 +0100

    upgraded emotion + storybook.

--- packages/grafana-ui/.storybook/main.ts
+++ packages/grafana-ui/.storybook/main.ts
@@ -1,15 +1,4 @@
 module.exports = {
   stories: ['../src/**/*.story.{js,jsx,ts,tsx,mdx}'],
-  addons: [
-    '@storybook/addon-knobs',
-    '@storybook/addon-actions',
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        configureJSX: true,
-        babelOptions: {},
-        sourceLoaderOptions: null,
-      },
-    },
-  ],
+  addons: ['@storybook/addon-knobs', '@storybook/addon-actions', '@storybook/addon-docs'],
 };
